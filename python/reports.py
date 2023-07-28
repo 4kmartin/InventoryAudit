b7901d07b1e5af33c1ce8571db31b3bda0c49e18
Anthany Martin <4kmartin@gmail.com>
2023-07-28 14:14:29 -0500

    fixed some bugs

--- python/reports.py
+++ python/reports.py
@@ -5,7 +5,7 @@ from sqlite3 import Connection
 def report_new_assets (connection:Connection) -> list[tuple]:
     today = date.today().toordinal()
     statement = f"SELECT hostname,fqdn,ip,mac FROM discovered_assets WHERE datefound = {today} EXCEPT SELECT hostname,fqdn,ip,mac FROM discovered_assets WHERE datefound < {today}"
-    _run_select_statement(connection, statement)
+    return _run_select_statement(connection, statement)
 
 def report_discrepencies (connection:Connection, data_sources:list[str]) -> list[tuple]:
     unique = []
@@ -14,5 +14,5 @@ def report_discrepencies (connection:Connection, data_sources:list[str]) -> list
     return unique
 
 def report_unique_to(connection:Connection, source:str) -> list[tuple]:
-    statement = f"SELECT hostname, fqdn, ip, mac FROM discovered_assets WHERE source = {source} EXCEPT SELECT hostname,fqdn,ip,mac FROM discovered_assets WHERE source != {source}"
+    statement = f"SELECT hostname, fqdn, ip, mac FROM discovered_assets WHERE source = '{source}' EXCEPT SELECT hostname,fqdn,ip,mac FROM discovered_assets WHERE source != '{source}'"
     return _run_select_statement(connection, statement)
