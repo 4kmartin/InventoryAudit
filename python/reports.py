5a8263e6d6183e1581b8c9ee18858b0e23c5af60
Anthany Martin <4kmartin@gmail.com>
2023-07-28 12:46:54 -0500

    fixed typo in select statement for report_new_assets()

--- python/reports.py
+++ python/reports.py
@@ -4,7 +4,7 @@ from sqlite3 import Connection
 
 def report_new_assets (connection:Connection) -> list[tuple]:
     today = date.today().toordinal()
-    statement = f"SELECT hostname,fqdn,ip,mac FROM discovered_assets WHERE date = {today} EXCEPT SELECT hostname,fqdn,ip,mac FROM discovered_assets WHERE date < {today}"
+    statement = f"SELECT hostname,fqdn,ip,mac FROM discovered_assets WHERE datefound = {today} EXCEPT SELECT hostname,fqdn,ip,mac FROM discovered_assets WHERE datefound < {today}"
     _run_select_statement(connection, statement)
 
 def report_discrepencies (data_sources:list[str]) -> list[tuple]:
