46284eed74e5a547023e80f038d534b77f2b836c
Anthany Martin <4kmartin@gmail.com>
2023-08-10 15:36:03 -0500

    added more reporting features

--- python/reports.py
+++ python/reports.py
@@ -1,5 +1,5 @@
 from datetime import date
-from python.dbMaintainance import _run_select_statement
+from python.dbMaintainance import _run_select_statement, collate_data, collate_from_source
 from sqlite3 import Connection
 from os.path import join
 
@@ -28,3 +28,14 @@ def write_report_to_file (report_name:str,query_output:list[tuple],description:s
         report_file.write(report)
         report_file.close()
         
+def report_not_in_source (connection:Connection, source:str) -> list[tuple]:
+    collate_data(connection,source)
+    statement = f"SELECT name FROM collated_assets WHERE name NOT IN (SELECT hostname FROM discovered_assets WHERE source = {source}) OR mac NOT IN (SELECT mac FROM discovered_assets WHERE source = {source});"
+    return _run_select_statement(statement)
+
+def report_compare_two_sources (connection:Connection, source_1:str, source_2:str) -> list[tuple]:
+    collate_from_source(connection,source_1)
+    collate_from_source(connection, source_2)
+    not_in_1 = f"SELECT name FROM collated_from_{source_2} WHERE name NOT IN collated_from_{source_1};"
+    not_in_2 = f"SELECT name FROM collated_from_{source_1} WHERE name NOT IN collated_from_{source_2};"
+    return _run_select_statement(connection,not_in_1) + _run_select_statement(connection,not_in_2)
