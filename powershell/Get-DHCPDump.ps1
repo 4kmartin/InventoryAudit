f877b3f8c0c973211918d9b3871b9551bca9cf36
Anthany Martin <4kmartin@gmail.com>
2023-08-16 13:35:41 -0500

    changed argumment order to better match Get-DnsDump.ps1

--- powershell/Get-DHCPDump.ps1
+++ powershell/Get-DHCPDump.ps1
@@ -1,5 +1,5 @@
-$OUTPUT_FILE= $args[0]
-$SERVER= $args[1]
+$OUTPUT_FILE= $args[1]
+$SERVER= $args[0]
 
 Get-DHCPServerv4Scope -ComputerName $SERVER | Get-DHCPServerv4Lease -ComputerName $SERVER | Select HostName, IPAddress | Export-CSV -Path $OUTPUT_FILE
 
