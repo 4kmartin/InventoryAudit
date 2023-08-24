fb12b2b2c9904748fa9045bf4fffdd997cf60370
Anthany Martin <4kmartin@gmail.com>
2023-08-24 12:24:13 -0500

    dropping empty string returns

--- python/tenableDump.py
+++ python/tenableDump.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 class TenableAsset (Asset):
 
-    def __init__(self, host_name:Optional[str], fqdn:Optional[str], mac_addr:Optional[str], ip_addr:Optional[str]) -> None:
+    def __init__(self, host_name:Optional[str], fqdn:Optional[str], ip_addr:Optional[str], mac_addr:Optional[str]) -> None:
         Asset.__init__(self, "Tenable", host_name, fqdn, ip_addr, mac_addr)
       
 
@@ -17,23 +17,23 @@ def filter_data (asset_list:list[dict]) ->list[TenableAsset]:
     for asset in asset_list:
         # print(asset["hostname"])
         try:
-            host_name = asset["hostname"][-1]
+            host_name = asset["hostname"][-1] if asset["hostname"] not in ("", " ") else None
         except IndexError:
             host_name = None
         try:
-            fqdn = asset["fqdn"][-1]
+            fqdn = asset["fqdn"][-1] if asset["fqdn"][-1] not in (""," ") else None
         except IndexError:
             fqdn = None
         try:
-            mac_addr = asset["mac_address"][-1]
+            mac_addr = asset["mac_address"][-1] if asset["mac_address"][-1] not in (""," ") else None
         except IndexError:
             mac_addr = None
         try:
-            ip_addr = asset["ipv4"][-1]
+            ip_addr = asset["ipv4"][-1] if asset["ipv4"][-1] not in ("", " ") else None
         except IndexError:
             ip_addr = None
         if (host_name,fqdn,mac_addr,ip_addr) == (None, None, None, None):
             continue
-        out.append(TenableAsset(host_name,fqdn,mac_addr,ip_addr))
+        out.append(TenableAsset(host_name,fqdn,ip_addr,mac_addr))
     return out
 
